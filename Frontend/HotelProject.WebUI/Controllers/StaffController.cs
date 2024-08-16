cabc793150f5d5a6a2852fa94e15bcf9988a3993
Furkan <furkannkayaa90@gmail.com>
2024-08-16 14:09:51 +0300

    Api Cors ve Consume İşlemleri - Personel Verilerinin Listelenmesi /Güncellenmesi

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
@@ -19,15 +21,113 @@ namespace HotelProject.WebUI.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("http://localhost:5453/api/Staff"); //verileri çektik 
-            if (responseMessage.IsSuccessStatusCode)
+            var client = _httpClientFactory.CreateClient();   //istemci oluşturduk
+            var responseMessage = await client.GetAsync("http://localhost:5453/api/Staff"); //ilgili adrese istekte bulunduk
             {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<StaffViwModel>>(jsonData); //tabloda gösterbilecek format
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();   //gelen veriyi jsondataya atadık
+                var values = JsonConvert.DeserializeObject<List<StaffViwModel>>(jsonData); //tabloda gösterbilecek formata dönüştürdük
                 return View(values);
             }
             return View();
         }
+        [HttpGet]
+        public ActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddStaff(AddStaffViewModel model)
+        {
+            var client= _httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData , Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5453/api/Staff", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public async Task<IActionResult> DeleteStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5453/api/Staff?id={id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        //public async Task<ActionResult> UpdateStaff(int id)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var responseMessage = await client.GetAsync($"http://localhost:5453/api/Staff?id={id}");
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        //        var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
+        //        return View(values);
+        //    }
+        //    return View();
+        //}
+
+
+        public async Task<ActionResult> UpdateStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5453/api/Staff?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<UpdateStaffViewModel>>(jsonData);
+                // İlgili staff'ı seçin; bu örnekte, varsayalım ilk elemanı alıyoruz
+                var staff = values.FirstOrDefault();
+                return View(staff);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateStaff(UpdateStaffViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model); // Hataları kullanıcıya göster
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PutAsync($"http://localhost:5453/api/Staff/{model.StaffId}", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index"); // Başarıyla güncellenince Index sayfasına yönlendir
+            }
+
+            // Hata durumunda, hata mesajını veya modelin kendisini geri döndür
+            ModelState.AddModelError("", "Güncelleme sırasında bir hata oluştu.");
+            return View(model);
+        }
+
+
+
+        //[HttpPost]
+        //public async Task<ActionResult> UpdateStaff(UpdateStaffViewModel model)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var jsonData=JsonConvert.SerializeObject(model);
+        //    StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        //    var responseMessage = await client.PutAsync("http://localhost:5453/api/Staff/",stringContent);
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
     }
 }
