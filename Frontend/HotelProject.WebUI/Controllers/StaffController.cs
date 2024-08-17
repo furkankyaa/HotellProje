53dfafd921fb6748e547f64398bffd43a8cbdc06
Furkan <furkannkayaa90@gmail.com>
2024-08-17 11:58:14 +0300

    Referans Controller (Referans İşlemleri)

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,4 +1,5 @@
 ﻿using HotelProject.WebUI.Models.Staff;
+using HotelProject.WebUI.Models.Testimonial;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
@@ -75,6 +76,20 @@ namespace HotelProject.WebUI.Controllers
         //}
 
 
+        //public async Task<ActionResult> UpdateStaff(int id)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var responseMessage = await client.GetAsync($"http://localhost:5453/api/Staff?id={id}");
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        //        var values = JsonConvert.DeserializeObject<List<UpdateStaffViewModel>>(jsonData);
+        //        // İlgili staff'ı seçin; bu örnekte, varsayalım ilk elemanı alıyoruz
+        //        var staff = values.FirstOrDefault();
+        //        return View(staff);
+        //    }
+        //    return View();
+        //}
         public async Task<ActionResult> UpdateStaff(int id)
         {
             var client = _httpClientFactory.CreateClient();
@@ -83,51 +98,52 @@ namespace HotelProject.WebUI.Controllers
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<UpdateStaffViewModel>>(jsonData);
-                // İlgili staff'ı seçin; bu örnekte, varsayalım ilk elemanı alıyoruz
-                var staff = values.FirstOrDefault();
+                // Belirli bir id'ye sahip staff'ı seçin
+                var staff = values.FirstOrDefault(s => s.StaffId == id);
                 return View(staff);
             }
             return View();
         }
 
+
+        [HttpPost]
+        //public async Task<ActionResult> UpdateStaff(UpdateStaffViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model); // Hataları kullanıcıya göster
+        //    }
+
+        //    var client = _httpClientFactory.CreateClient();
+        //    var jsonData = JsonConvert.SerializeObject(model);
+        //    var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        //    var responseMessage = await client.PutAsync($"http://localhost:5453/api/Staff/{model.StaffId}", stringContent);
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        return RedirectToAction("Index"); // Başarıyla güncellenince Index sayfasına yönlendir
+        //    }
+
+        //    // Hata durumunda, hata mesajını veya modelin kendisini geri döndür
+        //    ModelState.AddModelError("", "Güncelleme sırasında bir hata oluştu.");
+        //    return View(model);
+        //}
+
+
+
         [HttpPost]
         public async Task<ActionResult> UpdateStaff(UpdateStaffViewModel model)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model); // Hataları kullanıcıya göster
-            }
-
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(model);
-            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-            var responseMessage = await client.PutAsync($"http://localhost:5453/api/Staff/{model.StaffId}", stringContent);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5453/api/Staff/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
-                return RedirectToAction("Index"); // Başarıyla güncellenince Index sayfasına yönlendir
+
+                return RedirectToAction("Index");
             }
-
-            // Hata durumunda, hata mesajını veya modelin kendisini geri döndür
-            ModelState.AddModelError("", "Güncelleme sırasında bir hata oluştu.");
-            return View(model);
+            return View();
         }
-
-
-
-        //[HttpPost]
-        //public async Task<ActionResult> UpdateStaff(UpdateStaffViewModel model)
-        //{
-        //    var client = _httpClientFactory.CreateClient();
-        //    var jsonData=JsonConvert.SerializeObject(model);
-        //    StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-        //    var responseMessage = await client.PutAsync("http://localhost:5453/api/Staff/",stringContent);
-        //    if (responseMessage.IsSuccessStatusCode)
-        //    {
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View();
-        //}
     }
 }
---
+++ Frontend/HotelProject.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,98 @@
+﻿using HotelProject.WebUI.Models.Staff;
+using HotelProject.WebUI.Models.Testimonial;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();   //istemci oluşturduk
+            var responseMessage = await client.GetAsync("http://localhost:5453/api/Testimonial"); //ilgili adrese istekte bulunduk
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();   //gelen veriyi jsondataya atadık
+                var values = JsonConvert.DeserializeObject<List<TestimonialViewModel>>(jsonData); //tabloda gösterbilecek formata dönüştürdük
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddTestimonial(TestimonialViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5453/api/Testimonial", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:5453/api/Testimonial?id={id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<ActionResult> UpdateTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5453/api/Testimonial?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<TestimonialViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+       
+
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateTestimonial(TestimonialViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5453/api/Testimonial/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
