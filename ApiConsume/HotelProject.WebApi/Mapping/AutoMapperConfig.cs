ea33ed8ea60155eee79dc53fb5c078eab4105b3d
Furkan <furkannkayaa90@gmail.com>
2024-08-18 17:21:04 +0300

    Dto Katmanında Auto Mapper eklendi ve sonrasında odanın mapper kontrolü eklenmesi ve update dto testi yapıldı

---
+++ ApiConsume/HotelProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class AutoMapperConfig: Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDto, Room>();                    
+            CreateMap<Room, RoomAddDto>(); 
+
+            CreateMap<UpdateRoomDto,Room>().ReverseMap();  // reverse map tersine işliyor gerek kalmıyor daha yazmıyor 
+            
+        }
+    }
+}
