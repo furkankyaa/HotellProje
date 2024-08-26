226bf61a840d59e05c110108a9786f95ddb5ce89
Furkan <furkannkayaa90@gmail.com>
2024-08-26 16:32:11 +0300

    Rapid Api ile Listeleme (Parametre ile Verileri Çekme)

---
+++ RapidApi/RapidApiConsume/Models/ApiMovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RapidApiConsume.Models
+{
+    public class ApiMovieViewModel
+    {
+        public int Rank { get; set; }
+        public string Title { get; set; }
+        public string rating { get; set; }
+        public string trailer { get; set; }
+    }
+}
